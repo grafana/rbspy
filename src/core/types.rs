1e20a1795b02d70632ccbe3e949eab0d45a08cf2
Dmitry Filimonov <dmitry@pyroscope.io>
2021-02-16 20:54:35 -0800

    improves stack frame string format

--- src/core/types.rs
+++ src/core/types.rs
@@ -71,7 +71,7 @@ impl StackFrame {
 
 impl fmt::Display for StackFrame {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} - {}:{}", self.name, self.path(), self.lineno)
+        write!(f, "{}:{} - {}", self.relative_path, self.lineno, self.name)
     }
 }
 
